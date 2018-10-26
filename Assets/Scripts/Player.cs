f2d26aed7c1be27bd9acabf8df189a798230113d
ellier01234 <43067363+ellier01234@users.noreply.github.com>
2018-10-26 16:18:59 +0100

    Adding sprite animations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // designer variables 
+    public float speed = 10;
+    public Rigidbody2D physicsBody;
+    public string horizontalAxis = ("Horizontal");
+    public Animator playerAnimator;
+    public SpriteRenderer playerSprite;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // get axis input from unity 
+        float leftRight = Input.GetAxis("Horizontal");
+
+
+        // create direction vector from axis input 
+        Vector2 direction = new Vector2(leftRight, 0);
+
+        // make direction vector length 1 
+        direction = direction.normalized;
+
+        // calculate velocity 
+        Vector2 velocity = direction * speed;
+        velocity.y = physicsBody.velocity.y;
+
+        // give the velocity to the rigid body 
+        physicsBody.velocity = velocity;
+
+        // tell the animator our speed 
+        playerAnimator.SetFloat("walkSpeed", Mathf.Abs(velocity.x));
+
+        // flip our sprite if we're moving backward
+        if (velocity.x < 0)
+        {
+            playerSprite.flipX = true;
+
+        }
+        else
+        {
+            playerSprite.flipX = false;
+        }
+    }
+
+}
